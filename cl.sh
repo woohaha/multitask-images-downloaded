2c1175585898e1a576cb6aa1bc7b130cc4c78ea2
woohaha <realwoohaha@gmail.com>
2013-07-07 15:35:43 +0800

    改進標題獲取方式

--- cl.sh
+++ cl.sh
@@ -18,7 +18,7 @@
 #      REVISION:  ---
 #===============================================================================
 
-download_path='.'
+download_path="$HOME/163"
 
 [ -z $1 ] && {
 	echo '錯誤：沒有輸入網址'
@@ -30,7 +30,7 @@ echo -n '#'>> $download_path/$(basename -s .html $1).downloadlist
 echo $1 >> $download_path/$(basename -s .html $1).downloadlist
 
 echo -n '#'>> $download_path/$(basename -s .html $1).downloadlist
-title=$(grep -Poh '(?<=<title>).*(?=\[)' /tmp/$(basename $1) |enca -L zh_CN -x UTF-8)
+title=$(grep -Poh '(?<=<title>).*(?=\ +\-)' /tmp/$(basename $1) |enca -L zh_CN -x UTF-8 | sed 's/  草榴社區//')
 echo $title >> $download_path/$(basename -s .html $1).downloadlist
 grep -Poh '(?<=src.{2}).*?\.jpe?g' /tmp/$(basename $1) >> $download_path/$(basename -s .html $1).downloadlist
 
@@ -44,7 +44,7 @@ downloadlist() {
     while [ $ctl -le $lines ]; do
 	   if [ $(ps aux|grep wget|grep -v grep|wc -l) -le $tasks ];then
 		  ((ctl++))
-		  head -n $ctl $1|tail -n1|wget -i- -N -P $2 &
+		  head -n $ctl $1|tail -n1|wget --quiet -i- -N -P $2 &
 	   else
 		  continue
 	   fi
@@ -52,8 +52,10 @@ downloadlist() {
 }
 
 gallery_gen(){
-    ls $1/*.jp* | xargs -n1 -I% echo '<li><a href="%" rel="lightbox"><img class="gallery" src="%" alt="thumbnail image"/></a\></li>' >> $1/index.html
+    ls $1/*.jp* | awk -F'/' '{print $NF}'|xargs -n1 -I% echo '<li><a href="%" rel="lightbox"><img class="gallery" src="%" alt="thumbnail image"/></a\></li>' >> $1/index.html
 }
 
+echo '正在下載圖片...'
 downloadlist $download_path/$(basename -s .html $1).downloadlist $download_path/$title
 gallery_gen $download_path/$title
+echo 'Done!'
