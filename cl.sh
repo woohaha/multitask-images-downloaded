0b027ca1059213c42e8d7000fb7ef0966f460244
woohaha <realwoohaha@gmail.com>
2013-07-13 01:04:59 +0800

    改進了圖片擴展名不統一而出現無法下載與生成Gallry的錯誤

--- cl.sh
+++ cl.sh
@@ -24,15 +24,15 @@ download_path="$HOME/163/lll"
 	echo '錯誤：沒有輸入網址'
 	exit 1
 }
-wget -N -P /tmp $1
+wget -N -k -P /tmp $1
 
 echo -n '#'>> $download_path/$(basename -s .html $1).downloadlist
 echo $1 >> $download_path/$(basename -s .html $1).downloadlist
 
 echo -n '#'>> $download_path/$(basename -s .html $1).downloadlist
-title=$(grep -Poh '(?<=<title>).*(?=\ +\-)' /tmp/$(basename $1) |enca -L zh_CN -x UTF-8 | sed 's/  草榴社區//'|tr '\ ' '_')
+title=$(grep -Poh '(?<=<title>).*(?=\ +\-)' /tmp/$(basename $1) |enca -L zh_CN -x UTF-8 | sed 's/  草榴社區//'|tr '\[\]\ ' '_')
 echo $title >> $download_path/$(basename -s .html $1).downloadlist
-grep -Poh '(?<=src.{2}).*?\.jpe?g' /tmp/$(basename $1) >> $download_path/$(basename -s .html $1).downloadlist
+grep -Poh '(?<=src.{2}).*?\.[jJ][pP][eE]?[gG]' /tmp/$(basename $1) >> $download_path/$(basename -s .html $1).downloadlist
 
 
 #cat $download_path/$(basename -s .html $1).downloadlist|wget -i- -P $title
@@ -52,10 +52,10 @@ downloadlist() {
 }
 
 gallery_gen(){
-    ls $1/*.jp* | awk -F'/' '{print $NF}'|xargs -n1 -I% echo '<li><a href="%" rel="lightbox"><img class="gallery" src="%" alt="thumbnail image"/></a\></li>' >> $1/index.html
+    ls $1/* | grep -P '(([Jj][Pp][Ee]?[Gg])|([Pp][Nn][Gg]))' |awk -F'/' '{print $NF}'|xargs -n1 -I% echo '<li><a href="%" rel="lightbox"><img class="gallery" src="%" alt="thumbnail image"/></a\></li>' >> $1/index.html
 }
 
 echo '正在下載圖片...'
 downloadlist $download_path/$(basename -s .html $1).downloadlist $download_path/$title
-gallery_gen $download_path/$title
+gallery_gen "$download_path/$title"
 echo 'Done!'
