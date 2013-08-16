5e62b8df021f0feaa71cb6f0773c7b7cdcfb9f59
woohaha <realwoohaha@gmail.com>
2013-08-16 21:36:20 +0800

    我都忘了改過甚麼

--- cl.sh
+++ cl.sh
@@ -18,21 +18,44 @@
 #      REVISION:  ---
 #===============================================================================
 
-download_path="$HOME/163/lll"
+download_root_path="$HOME/163/lll"
+download_path=''
+while getopts p:r:s:w:u opt;do
+    case $opt in
+	   p)
+		  download_path="$download_root_path/pure"
+		  ;;
+	   r)
+		  download_path="$download_root_path/rosi"
+		  ;;
+	   s)
+		  download_path="$download_root_path/scute"
+		  ;;
+	   w)
+		  download_path="$download_root_path/white"
+		  ;;
+	   u)
+		  download_path="$download_root_path"
+		  ;;
+    esac
+done
+[ -z $download_path ] && download_path=$download_root_path
 
-[ -z $1 ] && {
+
+[ -z $OPTARG ] && OPTARG=$1
+[ -z $OPTARG ] && {
 	echo '錯誤：沒有輸入網址'
 	exit 1
 }
-wget -N -k -P /tmp $1
+wget -N -k -P /tmp $OPTARG
 
-echo -n '#'>> $download_path/$(basename -s .html $1).downloadlist
-echo $1 >> $download_path/$(basename -s .html $1).downloadlist
+echo -n '#'>> $download_path/$(basename -s .html $OPTARG).downloadlist
+echo $OPTARG >> $download_path/$(basename -s .html $OPTARG).downloadlist
 
-echo -n '#'>> $download_path/$(basename -s .html $1).downloadlist
-title=$(grep -Poh '(?<=<title>).*(?=\ +\-)' /tmp/$(basename $1) |enca -L zh_CN -x UTF-8 | sed 's/  草榴社區//'|tr '\[\]\ ' '_')
-echo $title >> $download_path/$(basename -s .html $1).downloadlist
-grep -Poh '(?<=src.{2}).*?\.[jJ][pP][eE]?[gG]' /tmp/$(basename $1) >> $download_path/$(basename -s .html $1).downloadlist
+echo -n '#'>> $download_path/$(basename -s .html $OPTARG).downloadlist
+title=$(grep -Poh '(?<=<title>).*(?=\ +\-)' /tmp/$(basename $OPTARG) |enca -L zh_CN -x UTF-8 | sed 's/  草榴社區//'|tr '\[\]\ ' '_')
+echo $title >> $download_path/$(basename -s .html $OPTARG).downloadlist
+grep -Poh '(?<=src.{2}).*?\.[jJ][pP][eE]?[gG]' /tmp/$(basename $OPTARG) >> $download_path/$(basename -s .html $OPTARG).downloadlist
 
 
 #cat $download_path/$(basename -s .html $1).downloadlist|wget -i- -P $title
@@ -59,3 +82,4 @@ echo '正在下載圖片...'
 downloadlist $download_path/$(basename -s .html $1).downloadlist $download_path/$title
 gallery_gen "$download_path/$title"
 echo 'Done!'
+rm /tmp/$(basename $OPTARG)
